5926108c3f8a0780dc6eeca00fad7ae18303b765
ac-optimus <abhavya.chandra@iitgn.ac.in>
2019-03-15 18:59:03 +0530

    adding random agent on LunarLander

--- lunarLander.py
+++ lunarLander.py
@@ -1,5 +1,16 @@
+#inspired from gym official website
+#random agent
+import time
 import gym
 env = gym.make('LunarLander-v2')
-env.reset()
-while True:
-    env.render()
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+       # print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        time.sleep(0.01)
+        if done:     #done is returned as true when termination condition(lander touches ground) occurs
+            print("Episode finished after {} timesteps".format(t+1))
+            break
