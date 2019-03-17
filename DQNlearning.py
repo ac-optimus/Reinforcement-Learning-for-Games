d3fb4ae9b775e54c7b7f7c5e515e7aa4da9b6e0e
ac-optimus <abhavya.chandra@iitgn.ac.in>
2019-03-17 21:17:08 +0530

    changing learning rate

--- DQNlearning.py
+++ DQNlearning.py
@@ -4,15 +4,16 @@ import numpy as np
 from collections import deque
 import torch.nn as nn
 import torch
+import time
 
-ENV_NAME = "LunarLander-v2"
+ENV_NAME = "CartPole-v1"
 
 GAMMA = 0.99
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.0005
 
 MEMORY_SIZE = 1000000
 BATCH_SIZE = 20
-
+    
 EXPLORATION_MAX = 1.0
 EXPLORATION_MIN = 0.01
 EXPLORATION_DECAY = 0.995
@@ -22,7 +23,7 @@ class Netx(nn.Module):
     def __init__(self, observation_space, action_space):
         super().__init__()
 
-        hiddenlayers = 24
+        hiddenlayers = 64
 
         self.fc1 = nn.Linear(observation_space, hiddenlayers)
         self.bn1 = nn.BatchNorm1d(hiddenlayers)
@@ -61,7 +62,7 @@ class DQNSolver:
 
         ## Model and the optimizer
         self.model = Netx(observation_space, action_space)
-      #  self.model.double()
+        self.model.double()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=LEARNING_RATE)
         self.criterion = nn.MSELoss()
 
@@ -96,7 +97,7 @@ class DQNSolver:
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
-        self.exploration_rate *= EXPLORATION_DECAY
+        self.exploration_rate *= EXPLORATION_DECAY**2
         self.exploration_rate = max(EXPLORATION_MIN, self.exploration_rate)
 
 def inference():
@@ -112,8 +113,12 @@ def inference():
         step = 0
         while True:
             step += 1
+            time.sleep(0.01)
             env.render()
-            action = DQNSolver(observation_space, action_space).act(state)
+            with torch.no_grad():
+                q_values = model(torch.tensor(state))
+                action = torch.argmax(q_values[0]).item()
+            #action = DQNSolver(observation_space, action_space).act(state)
             state_next, reward, terminal, info = env.step(action)
             reward = reward if not terminal else -reward
             state_next = np.reshape(state_next, [1, observation_space])
@@ -129,7 +134,11 @@ def train():
     action_space = env.action_space.n
     dqn_solver = DQNSolver(observation_space, action_space)
     num_epsoid = 0
-    while num_epsoid < 5:
+    l = []
+    start = time.time()
+    while num_epsoid < 200:
+        if len(l)>8:
+            break
         num_epsoid += 1
         state = env.reset()
         state = np.reshape(state, [1, observation_space])
@@ -145,13 +154,17 @@ def train():
             state = state_next
             if terminal:
                 print ("Epsoid: " + str(num_epsoid) + ", exploration: " + str(dqn_solver.exploration_rate) + ", score: " + str(step))
+                if step >=150:
+                    l.append(step)
                 break
             dqn_solver.experience_replay()
+    end = time.time()
+    print ("time taken to train: ",end-start)
     dqn_solver.save_model()
 
 
 
 if __name__ == "__main__":
-    train()
-    print ("---------------------inferencing now---------------------------")
+    #train()
+    #print ("---------------------inferencing now---------------------------")
     inference()
