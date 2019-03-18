27bf516233c7e6534dadcd05cded7e3c00821f17
Apoorv Ubu18 <apoorvagni@hotmail.com>
2019-03-19 00:06:33 +0530

    added classic Q

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ After working on our environment, we tried using Q-Learning on [Berkley AI Cours
 
 ## Files
 - `personal_game.ipynb`: our take on creating an environment to understand rewards, states, actions, etc. better.<br>
-- `DQNlearning.py`: file that tries to train an agent to play CartPool-v1 from OpenAI gym using the observation space vector and a fully connected neural network.<br> The code uses the base code at [gsurma/cartpole][3] and is ported to use PyTorch with some experimentations on the network architecture.
+- `DQNlearning.py`: file that tries to train an agent to play `CartPool-v1` from OpenAI gym using the observation space vector and a fully connected neural network.<br> The code uses the base code at [gsurma/cartpole][3] and is ported to use PyTorch with some experimentations on the network architecture.
+- `classicQ.py`: trying to run `CartPool-v1` with the help of classic Q Learning algorithm by discretizing the continuous observation space. Inspired from [`qcartpole.py`][6].
 
 We plan to look at the following policies to try to train our agents:
-- [x] Q-learning
+- [x] Q-learning on Berkley Pacman
+- [x] Q-learning on Cartpool
 - [x] Deep Q Network directly using observation space vector (if available)
 - [ ] Deep Q Network using convolutions on the render of the environment
 - [ ] Double Q Learning
@@ -25,9 +27,12 @@ Plan to use Deep Q Network using CNNs on CartPool-v1 and add some graphs showcas
 - [PyTorch][4]
 - [Berkley AI Course][2]
 - [gsurma/cartpole][3]
+- [Pytorch Dual DQN][5]
+- [Classical Q Learning on Cartpool][6]
 
   [1]: https://gym.openai.com/
   [2]: http://ai.berkeley.edu/home.html
   [3]: https://github.com/gsurma/cartpole
   [4]: https://pytorch.org/
-  
+  [5]: https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+  [6]: https://gist.github.com/n1try/af0b8476ae4106ec098fea1dfe57f578#file-qcartpole-py-L24
---
+++ classicQ.py
@@ -0,0 +1,96 @@
+"""Inspired by https://gist.github.com/n1try/af0b8476ae4106ec098fea1dfe57f578"""
+
+import gym
+import numpy as np
+import math
+from collections import deque
+
+class ObsDesc:
+    def __init__(self, env, buckets=(1, 1, 6, 12,), up=None, low=None):
+        self.env = env
+        self.buckets = buckets
+        if up is None or low is None:
+            self.up  = [self.env.observation_space.high[0], 0.5,
+                        self.env.observation_space.high[2], math.radians(50)]
+            self.low = [self.env.observation_space.low[0], -0.5,
+                        self.env.observation_space.low[2], -math.radians(50)]
+        else:
+            self.up = up
+            self.low = low
+        assert len(self.up) == len(self.low)
+
+    def discretize(self, observation):
+        ratios = [(observation[i] + abs(self.low[i])) / (self.up[i] - self.low[i]) for i in range(len(self.low))]
+        new_obs = [int(round((self.buckets[i] - 1) * ratios[i])) for i in range(len(observation))]
+        new_obs = [min(self.buckets[i] - 1, max(0, new_obs[i])) for i in range(len(observation))]
+        return tuple(new_obs)
+
+
+class QCartPoleSolver():
+    def __init__(self, n_episodes=1000, n_win_ticks=195,
+                 min_alpha=0.1, min_epsilon=0.1,
+                 gamma=1.0, ada_divisor=25, max_env_steps=None,
+                 quiet=False, monitor=False):
+        self.n_episodes = n_episodes # training episodes 
+        self.n_win_ticks = n_win_ticks # average ticks over 100 episodes required for win
+        self.min_alpha = min_alpha # learning rate
+        self.min_epsilon = min_epsilon # exploration rate
+        self.gamma = gamma # discount factor
+        self.ada_divisor = ada_divisor # only for development purposes
+        self.quiet = quiet
+        self.env = gym.make('CartPole-v0')
+        self.obscretizer = ObsDesc(self.env)
+
+        if max_env_steps is not None: self.env._max_episode_steps = max_env_steps
+        if monitor: self.env = gym.wrappers.Monitor(self.env, 'tmp/cartpole-1', force=True) # record results for upload
+
+        self.Q = np.zeros(self.obscretizer.buckets + (self.env.action_space.n,))
+
+    def choose_action(self, state, epsilon):
+        return self.env.action_space.sample() if (np.random.random() <= epsilon) else np.argmax(self.Q[state])
+
+    def update_q(self, state_old, action, reward, state_new, alpha):
+        self.Q[state_old][action] += alpha * (reward + self.gamma * np.max(self.Q[state_new]) - self.Q[state_old][action])
+
+    def get_epsilon(self, t):
+        return max(self.min_epsilon, min(1, 1.0 - math.log10((t + 1) / self.ada_divisor)))
+
+    def get_alpha(self, t):
+        return max(self.min_alpha, min(1.0, 1.0 - math.log10((t + 1) / self.ada_divisor)))
+
+    def run(self):
+        scores = deque(maxlen=100)
+
+        for e in range(self.n_episodes):
+            current_state = self.obscretizer.discretize(self.env.reset())
+
+            alpha = self.get_alpha(e)
+            epsilon = self.get_epsilon(e)
+            done = False
+            i = 0
+
+            while not done:
+                # self.env.render()
+                action = self.choose_action(current_state, epsilon)
+                obs, reward, done, _ = self.env.step(action)
+                new_state = self.obscretizer.discretize(obs)
+                self.update_q(current_state, action, reward, new_state, alpha)
+                current_state = new_state
+                i += 1
+
+            scores.append(i)
+            mean_score = np.mean(scores)
+            if mean_score >= self.n_win_ticks and e >= 100:
+                if not self.quiet: 
+                    print('Ran {} episodes. Solved after {} trials ✔'.format(e, e - 100))
+                return e - 100
+            if e % 100 == 0 and not self.quiet:
+                print('[Episode {}] - Mean survival time over last 100 episodes was {} ticks.'.format(e, mean_score))
+
+        if not self.quiet: print('Did not solve after {} episodes 😞'.format(e))
+        return e
+
+if __name__ == "__main__":
+    solver = QCartPoleSolver()
+    solver.run()
+    # gym.upload('tmp/cartpole-1', api_key='')
